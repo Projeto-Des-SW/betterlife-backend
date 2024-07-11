46af357d2ff7c055dc117ee10255944d599260e4
Bielfla27 <gabibielmft@outlook.com>
2024-07-11 19:15:54 -0300

    Inicialização do projeto

---
+++ Betterlife/src/main/java/com/example/Betterlife/BetterlifeApplication.java
@@ -0,0 +1,13 @@
+package com.example.Betterlife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetterlifeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BetterlifeApplication.class, args);
+	}
+
+}
---
+++ Betterlife/src/test/java/com/example/Betterlife/BetterlifeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Betterlife;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BetterlifeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
