2f6f881a2de8059c025d4cf3fcf6a1062add74c2
Daniel Duarte <danielduartefilho.df@gmail.com>
2024-08-30 20:11:44 -0300

    removido parametro de animal id do endpoint de salvar foto

--- src/controllers/imagensController.js
+++ src/controllers/imagensController.js
@@ -1,11 +1,11 @@
 const pool = require('../../config/db');
 
 exports.salvarFotos = async (req, res) => {
-    const { animalId, arquivofoto, nomearquivo } = req.body;
+    const {arquivofoto, nomearquivo } = req.body;
 
-    if (!animalId || !arquivofoto || !nomearquivo) {
+    if (!arquivofoto || !nomearquivo) {
         return res.status(400).json({ 
-            error: 'ID do animal, arquivo da foto e nome do arquivo são obrigatórios.', 
+            error: 'Arquivo da foto e nome do arquivo são obrigatórios.', 
             dados: req.body 
         });
     }
@@ -16,12 +16,12 @@ exports.salvarFotos = async (req, res) => {
         const dataCriacao = new Date();
 
         const queryText = `
-            INSERT INTO imagens (animalid, arquivofoto, nomearquivo, datacriacao)
-            VALUES ($1, $2, $3, $4)
+            INSERT INTO imagens (arquivofoto, nomearquivo, datacriacao)
+            VALUES ($1, $2, $3)
             RETURNING *;
         `;
 
-        const result = await client.query(queryText, [animalId, arquivofoto, nomearquivo, dataCriacao]);
+        const result = await client.query(queryText, [arquivofoto, nomearquivo, dataCriacao]);
 
         client.release();
 
